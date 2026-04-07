64dc2a6620a794c515fef50a32b6965b0366815f
Հայկ <hayk.zaqaryan5@edu.ysu.am>
2026-04-07 17:09:45 +0400

    Update socialbenefit_by_family_part_2.sql

--- socialbenefit_by_family_part_2.sql
+++ socialbenefit_by_family_part_2.sql
@@ -71,12 +71,16 @@ SELECT
     (COALESCE("HH"."HH_Income_Subtotal", 0) + COALESCE("App"."LivestockIncome", 0) + COALESCE("HH"."73", 0)) / NULLIF("Eval"."AdultEquivalent", 0) AS "76", 
     (34581 - COALESCE("Eval"."AdultEquivalentMonthlyIncome", 0)) AS "77", 
     CASE 
-        WHEN EXTRACT(YEAR FROM "App"."DateSubmitted") = 2026 THEN
-            CASE WHEN (("Eval"."AdultEquivalentMonthlyIncome" + "HH"."73") / NULLIF("Eval"."AdultEquivalent", 0)) >= 35875.00 THEN 1 ELSE 0 END
-        WHEN EXTRACT(YEAR FROM "App"."DateSubmitted") = 2025 THEN
-            CASE WHEN (("Eval"."AdultEquivalentMonthlyIncome" + "HH"."73") / NULLIF("Eval"."AdultEquivalent", 0)) >= 34581.00 THEN 1 ELSE 0 END
+        WHEN ("R3"."ApplicationID" IS NOT NULL OR "S3"."ApplicationID" IS NOT NULL) THEN
+            CASE 
+                WHEN EXTRACT(YEAR FROM "App"."DateSubmitted") = 2026 THEN
+                    CASE WHEN "Eval"."AdultEquivalentMonthlyIncome" - (COALESCE("HH"."73", 0) / NULLIF("Eval"."AdultEquivalent", 0)) < 35875.00 THEN 1 ELSE 0 END
+                WHEN EXTRACT(YEAR FROM "App"."DateSubmitted") = 2025 THEN
+                    CASE WHEN "Eval"."AdultEquivalentMonthlyIncome" - (COALESCE("HH"."73", 0) / NULLIF("Eval"."AdultEquivalent", 0)) < 34581.00 THEN 1 ELSE 0 END
+                ELSE 0 
+            END
         ELSE 0 
-    END AS "101",
+    END AS "101,
 
     -- Rejection & Inspection Logic
     COALESCE("Rej"."Is_Only_8", 0) AS "102",
